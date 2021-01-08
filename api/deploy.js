4a88b98dcd20f07439c62ac5e9b409d8d3cc6d56
Kate Sills <kate@agoric.com>
2021-01-07 16:35:14 -0800

    chore: get basic deploy scripts working (#51)

--- api/deploy.js
+++ api/deploy.js
@@ -3,186 +3,104 @@
 
 import fs from 'fs';
 import { E } from '@agoric/eventual-send';
-import makeAmountMath from '@agoric/ertp/src/amountMath';
-import dappConstants from '../ui/src/utils/constants';
+import '@agoric/zoe/exported';
+import bundleSource from '@agoric/bundle-source';
+import { makeHelpers } from '@agoric/deploy-script-support';
 
-// deploy.js runs in an ephemeral Node.js outside of swingset. The
-// spawner runs within ag-solo, so is persistent.  Once the deploy.js
-// script ends, connections to any of its objects are severed.
+import installationConstants from '../ui/public/conf/installationConstants';
 
-/**
- * @typedef {Object} DeployPowers The special powers that `agoric deploy` gives us
- * @property {(path: string) => { moduleFormat: string, source: string }} bundleSource
- * @property {(path: string) => string} pathResolve
- */
+const API_PORT = process.env.API_PORT || '8000';
 
-/**
- * @param {any} referencesPromise A promise for the references
- * available from REPL home
- * @param {DeployPowers} powers
- */
-export default async function deployApi(
-  referencesPromise,
-  { bundleSource, pathResolve },
-) {
-  // Let's wait for the promise to resolve.
-  const references = await referencesPromise;
+export default async function deployApi(homePromise, endowments) {
+  const { board, priceAuthority, zoe, http, spawner } = E.G(homePromise);
+  const helpers = await makeHelpers(homePromise, endowments);
 
-  // Unpack the references.
   const {
-    // *** LOCAL REFERENCES ***
+    INSTALLATION_BOARD_ID,
+    AUTOSWAP_INSTALLATION_BOARD_ID,
+    CONTRACT_NAME,
+  } = installationConstants;
 
-    // This wallet only exists on this machine, and only you have
-    // access to it. The wallet stores purses and handles transactions.
-    wallet,
-
-    // Scratch is a map only on this machine, and can be used for
-    // communication in objects between processes/scripts on this
-    // machine.
-    uploads: _scratch,
-
-    // The spawner persistently runs scripts within ag-solo, off-chain.
-    spawner,
-
-    // *** ON-CHAIN REFERENCES ***
-
-    // Zoe lives on-chain and is shared by everyone who has access to
-    // the chain. In this demo, that's just you, but on our testnet,
-    // everyone has access to the same Zoe.
-    zoe,
-
-    // The registry also lives on-chain, and is used to make private
-    // objects public to everyone else on-chain. These objects get
-    // assigned a unique string key. Given the key, other people can
-    // access the object through the registry
-    registry,
-
-    // The http request handler.
-    // TODO: add more explanation
-    http,
-  } = references;
-
-  // To get the backend of our dapp up and running, first we need to
-  // grab the installationHandle that our contract deploy script put
-  // in the public registry.
-  const { INSTALLATION_REG_KEY } = dappConstants;
-  const autoswapContractInstallationHandle = await E(registry).get(
-    INSTALLATION_REG_KEY,
-  );
-
-  // Second, we can use the installationHandle to create a new
-  // instance of our contract code on Zoe. A contract instance is a running
-  // program that can take offers through Zoe. Creating a contract
-  // instance gives you an invitation to the contract. In this case, it is
-  // an invitation to add liquidity to the liquidity pool.
-
-  // At the time that we make the contract instance, we need to tell
-  // Zoe what kind of token to accept as the two liquidity tokens. In
-  // this instance, we will only accept moola and simoleans. (If we
-  // wanted to accept other kinds of tokens, we could create other
-  // instances.) We need to put this information in the form of a
-  // keyword (a string that the contract determines, in this case,
-  // 'TokenA' and 'TokenB') plus the issuers for the token kinds, the
-  // moolaIssuer and simoleanIssuer.
-
-  // In our example, moola and simoleans are widely used tokens.
-  // Someone has already registered the moolaIssuer and the
-  // simolenIssuer in the registry. We could also get it from our
+  // use the board id to get the values, and save them to the
   // wallet.
-
-  // getIssuers returns an array, because we currently cannot
-  // serialize maps. We can immediately create a map using the array,
-  // though. https://github.com/Agoric/agoric-sdk/issues/838
-  const issuersArray = await E(wallet).getIssuers();
-  const issuers = new Map(issuersArray);
-  const moolaIssuer = issuers.get('moola');
-  const simoleanIssuer = issuers.get('simolean');
-
-  const getLocalAmountMath = issuer =>
-    Promise.all([
-      E(issuer).getBrand(),
-      E(issuer).getMathHelpersName(),
-    ]).then(([brand, mathHelpersName]) =>
-      makeAmountMath(brand, mathHelpersName),
-    );
-
-  const moolaAmountMath = await getLocalAmountMath(moolaIssuer);
-  const simoleanAmountMath = await getLocalAmountMath(simoleanIssuer);
-
-  const issuerKeywordRecord = { TokenA: moolaIssuer, TokenB: simoleanIssuer };
-  const {
-    invitation: addLiquidityInvitation,
-    instanceRecord: { publicAPI, handle: instanceHandle },
-  } = await E(zoe).makeInstance(
-    autoswapContractInstallationHandle,
-    issuerKeywordRecord,
+  const autoswapInstall = await E(board).getValue(
+    AUTOSWAP_INSTALLATION_BOARD_ID,
   );
+  const stablecoinMachineInstallation = await E(board).getValue(
+    INSTALLATION_BOARD_ID,
+  );
+
+  const terms = harden({
+    autoswapInstall,
+    priceAuthoritySource: priceAuthority,
+  });
+
+  const startInstanceConfig = {
+    instancePetname: CONTRACT_NAME,
+    installation: stablecoinMachineInstallation,
+    terms,
+    issuerKeywordRecord: {},
+  };
+
+  const { instance, creatorFacet } = await helpers.startInstance(
+    startInstanceConfig,
+  );
+
   console.log('- SUCCESS! contract instance is running on Zoe');
 
-  const liquidityIssuer = await E(publicAPI).getLiquidityIssuer();
-  const liquidityAmountMath = await getLocalAmountMath(liquidityIssuer);
+  console.log('Retrieving Board IDs for issuers and brands');
+  const invitationIssuerP = E(zoe).getInvitationIssuer();
+  const invitationBrandP = E(invitationIssuerP).getBrand();
 
-  // Let's add liquidity using our wallet and the addLiquidityInvitation
-  // we have.
-  const proposal = {
-    give: {
-      TokenA: moolaAmountMath.make(900),
-      TokenB: simoleanAmountMath.make(500),
-    },
-    want: {
-      Liquidity: liquidityAmountMath.getEmpty(),
-    },
+  const invitationIssuer = await invitationIssuerP;
+
+  const INSTANCE_BOARD_ID = await E(board).getId(instance);
+
+  console.log(`-- Contract Name: ${CONTRACT_NAME}`);
+  console.log(`-- INSTANCE_BOARD_ID: ${INSTANCE_BOARD_ID}`);
+
+  const installURLHandler = async () => {
+    const bundle = await bundleSource(
+      helpers.resolvePathForLocalContract('./src/handler.js'),
+    );
+
+    // Install it on the spawner
+    const handlerInstall = E(spawner).install(bundle);
+
+    // Spawn the installed code to create an URL handler.
+    const handler = E(handlerInstall).spawn({
+      creatorFacet,
+      board,
+      http,
+      invitationIssuer,
+    });
+
+    // Have our ag-solo wait on ws://localhost:8000/api/card-store for
+    // websocket connections.
+    await E(http).registerURLHandler(handler, '/api/card-store');
   };
 
-  const pursesArray = await E(wallet).getPurses();
-  const purses = new Map(pursesArray);
+  await installURLHandler();
 
-  const moolaPurse = purses.get('Fun budget');
-  const simoleanPurse = purses.get('Nest egg');
+  const invitationBrand = await invitationBrandP;
+  const INVITE_BRAND_BOARD_ID = await E(board).getId(invitationBrand);
 
-  const moolaPayment = await E(moolaPurse).withdraw(proposal.give.TokenA);
-  const simoleanPayment = await E(simoleanPurse).withdraw(proposal.give.TokenB);
-
-  const payments = {
-    TokenA: moolaPayment,
-    TokenB: simoleanPayment,
-  };
-  const { outcome, _payout } = await E(zoe).offer(
-    addLiquidityInvitation,
-    proposal,
-    payments,
-  );
-  console.log(await outcome);
-
-  // Now that we've done all the admin work, let's share this
-  // instanceHandle by adding it to the registry. Any users of our
-  // contract will use this instanceHandle to get invitations to the
-  // contract in order to make an offer.
-  const INSTANCE_REG_KEY = await E(registry).register(
-    `${dappConstants.CONTRACT_NAME}instance`,
-    instanceHandle,
-  );
-
-  console.log(`-- Contract Name: ${dappConstants.CONTRACT_NAME}`);
-  console.log(`-- InstanceHandle Register Key: ${INSTANCE_REG_KEY}`);
-
-  const bundle = await bundleSource(pathResolve('./src/handler.js'));
-  const handlerInstall = E(spawner).install(bundle);
-
-  const handler = E(handlerInstall).spawn({ registry, publicAPI });
-
-  await E(http).registerAPIHandler(handler);
+  const API_URL = process.env.API_URL || `http://127.0.0.1:${API_PORT || 8000}`;
 
   // Re-save the constants somewhere where the UI and api can find it.
-  const newDappConstants = {
-    INSTANCE_REG_KEY,
-    ...dappConstants,
+  const dappConstants = {
+    INSTANCE_BOARD_ID,
+    INSTALLATION_BOARD_ID,
+    INVITE_BRAND_BOARD_ID,
+    // BRIDGE_URL: 'agoric-lookup:https://local.agoric.com?append=/bridge',
+    BRIDGE_URL: 'http://127.0.0.1:8000',
+    API_URL,
   };
-  const defaultsFile = pathResolve(`../ui/src/utils/defaults.js`);
+  const defaultsFile = endowments.pathResolve(`../ui/public/conf/defaults.js`);
   console.log('writing', defaultsFile);
   const defaultsContents = `\
-  // GENERATED FROM ${pathResolve('./deploy.js')}
-  export default ${JSON.stringify(newDappConstants, undefined, 2)};
-  `;
+// GENERATED FROM ${endowments.pathResolve('./deploy.js')}
+export default ${JSON.stringify(dappConstants, undefined, 2)};
+`;
   await fs.promises.writeFile(defaultsFile, defaultsContents);
 }
--- contract/deploy.js
+++ contract/deploy.js
@@ -1,102 +1,46 @@
 // @ts-check
 import fs from 'fs';
-import { E } from '@agoric/eventual-send';
+import { makeHelpers } from '@agoric/deploy-script-support';
 
-// This script takes our contract code, installs it on Zoe, and makes
-// the installation publicly available. Our backend API script will
-// use this installation in a later step.
+import '@agoric/deploy-script-support/exported';
 
-/**
- * @typedef {Object} DeployPowers The special powers that agoric deploy gives us
- * @property {(path: string) => { moduleFormat: string, source: string }} bundleSource
- * @property {(path: string) => string} pathResolve
- */
-
-/**
- *
- * @param {*} referencesPromise
- * @param {DeployPowers} powers
- */
-export default async function deployContract(
-  referencesPromise,
-  { bundleSource, pathResolve },
-) {
-  // Your off-chain machine (what we call an ag-solo) starts off with
-  // a number of references, some of which are shared objects on chain, and
-  // some of which are objects that only exist on your machine.
-
-  // Let's wait for the promise to resolve.
-  const references = await referencesPromise;
-
-  // Unpack the references.
-  const {
-    // *** ON-CHAIN REFERENCES ***
-
-    // Zoe lives on-chain and is shared by everyone who has access to
-    // the chain. In this demo, that's just you, but on our testnet,
-    // everyone has access to the same Zoe.
-    zoe,
-
-    // The registry also lives on-chain, and is used to make private
-    // objects public to everyone else on-chain. These objects get
-    // assigned a unique string key. Given the key, other people can
-    // access the object through the registry.
-    registry,
-  } = references;
-
-  // First, we must bundle up our contract code (./src/contract.js)
-  // and install it on Zoe. This returns an installationHandle, an
-  // opaque, unforgeable identifier for our contract code that we can
-  // reuse again and again to create new, live contract instances.
-  const bundle = await bundleSource(pathResolve(`./src/contract.js`));
-  const installationHandle = await E(zoe).install(bundle);
-
-  // Let's share this installationHandle with other people, so that
-  // they can run our Autoswap contract code by making a contract
-  // instance (see the api deploy script in this repo to see an
-  // example of how to use the installationHandle to make a new contract
-  // instance.)
-
-  // To share the installationHandle, we're going to put it in the
-  // registry. The registry is a shared, on-chain object that maps
-  // strings to objects. We will need to provide a starting name when
-  // we register our installationHandle, and the registry will add a
-  // suffix creating a guaranteed unique name.
-  const CONTRACT_NAME = 'autoswap';
-  const INSTALLATION_REG_KEY = await E(registry).register(
-    `${CONTRACT_NAME}installation`,
-    installationHandle,
+export default async function deployContract(homePromise, endowments) {
+  // This installs the contract code on Zoe and adds the installation
+  // to the contract developer's wallet and the board
+  const helpers = await makeHelpers(homePromise, endowments);
+  const resolvedPath = helpers.resolvePathForLocalContract(
+    './src/stablecoinMachine',
   );
-  console.log('- SUCCESS! contract code installed on Zoe');
-  console.log(`-- Contract Name: ${CONTRACT_NAME}`);
-  console.log(`-- InstallationHandle Register Key: ${INSTALLATION_REG_KEY}`);
-
-  // Save the instanceId somewhere where the UI can find it.
-  const dappConstants = {
-    BRIDGE_URL: 'agoric-lookup:https://local.agoric.com?append=/bridge',
-    API_URL: '/',
-    INSTALLATION_REG_KEY,
+  const CONTRACT_NAME = 'stablecoinMachine';
+  const { id: INSTALLATION_BOARD_ID } = await helpers.install(
+    resolvedPath,
     CONTRACT_NAME,
-  };
-  const dc = 'dappConstants.js';
-  console.log('writing', dc);
-  await fs.promises.writeFile(
-    dc,
-    `globalThis.__DAPP_CONSTANTS__ = ${JSON.stringify(
-      dappConstants,
-      undefined,
-      2,
-    )}`,
   );
 
-  // Now add URLs so that development functions without internet access.
-  dappConstants.BRIDGE_URL = 'http://127.0.0.1:8000';
-  dappConstants.API_URL = 'http://127.0.0.1:8000';
-  const defaultsFile = pathResolve(`../ui/src/utils/defaults.js`);
+  // Install Autoswap
+  // TODO: install autoswap already in bootstrap.js in cosmic-swingset
+  const { id: AUTOSWAP_INSTALLATION_BOARD_ID } = await helpers.install(
+    helpers.resolvePathForPackagedContract(
+      '@agoric/zoe/src/contracts/multipoolAutoswap/multipoolAutoswap',
+    ),
+    'multipoolAutoswap',
+  );
+
+  // Save the constants somewhere where the UI and api can find it.
+  const dappConstants = {
+    CONTRACT_NAME,
+    INSTALLATION_BOARD_ID,
+    AUTOSWAP_INSTALLATION_BOARD_ID,
+  };
+  const defaultsFolder = endowments.pathResolve(`../ui/public/conf`);
+  const defaultsFile = endowments.pathResolve(
+    `../ui/public/conf/installationConstants.js`,
+  );
   console.log('writing', defaultsFile);
   const defaultsContents = `\
-// GENERATED FROM contract/deploy.js
+// GENERATED FROM ${endowments.pathResolve('./deploy.js')}
 export default ${JSON.stringify(dappConstants, undefined, 2)};
 `;
+  await fs.promises.mkdir(defaultsFolder, { recursive: true });
   await fs.promises.writeFile(defaultsFile, defaultsContents);
 }
