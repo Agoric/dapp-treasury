6ce03595dab978e9681893535e66513be9579ab4
Kris Kowal <kris@cixar.com>
2021-03-25 16:11:30 -0700

    fix: Externalize UI SES lockdown
     - work-around react-scripts: add <script> tags for lockdown
       in post-processing step.
     - invoke SES non-security mode for create-react-app compat.
       - include lockdown shim with prototype create-react-app compat kludge
     - monkey-patch Math.pow to handle bigints
       because some part of the react build tools
       rewrites 10n ** 2n as Math.pow(10n, 2n)
       - avoid ** rewrite by using newer browserlist in production
         (not sure this actually had any impact on **)
     - allow .constructor assignment using severe overrideTaming
     - in dev mode, don't call lockdown() again

---
+++ ui/ses-patch.js
@@ -0,0 +1,39 @@
+/* global process, require, module */
+// @ts-check
+const patch = {
+  target: '<link name="unprocessed-script" content="lockdown.umd.js"/>',
+  replacement: `
+    <script src="lockdown.umd.js"></script>
+    <script>
+    const { pow: mathPow } = Math;
+    Math.pow = (base, exp) => (typeof base === 'bigint' && typeof exp ==='bigint') ? base ** exp : mathPow(base, exp);
+    lockdown({ skipHardenIntrinsics: true, errorTaming: 'unsafe', overrideTaming: 'severe' });
+    console.log("lockdown done.");
+    </script>
+    `,
+};
+
+/**
+ *
+ * @param {string[]} args
+ * @param {{
+ *   readFile: typeof import('fs').promises.readFile,
+ *   writeFile: typeof import('fs').promises.writeFile,
+ * }} io
+ */
+async function main(args, { readFile, writeFile }) {
+  const [filename] = args;
+  const original = await readFile(filename, 'utf-8');
+  if (original.indexOf(patch.target) < 0) {
+    throw Error('target not found');
+  }
+  const patched = original.replace(patch.target, patch.replacement);
+  await writeFile(filename, patched);
+}
+
+if (require.main === module) {
+  // eslint-disable-next-line global-require
+  main(process.argv.slice(2), require('fs').promises).catch(err =>
+    console.error(err),
+  );
+}
--- ui/src/install-ses-lockdown.js
+++ ui/src/install-ses-lockdown.js
@@ -1,7 +1,26 @@
+/* global process */
 import 'ses/lockdown';
 import '@agoric/eventual-send/shim';
 
-lockdown({ errorTaming: 'unsafe' });
+function ifDevMode(thunk) {
+  if (typeof process !== 'undefined' && typeof process.env === 'object') {
+    console.log({ NODE_ENV: process.env.NODE_ENV });
+
+    if (process.env.NODE_ENV === 'development') {
+      thunk();
+    }
+  }
+}
+
+// In production, to avoid react-scripts / webpack rewriting SES,
+// we call lockdown() elsewhere. See ses-patch.js.
+ifDevMode(() =>
+  lockdown({
+    skipHardenIntrinsics: true,
+    errorTaming: 'unsafe',
+    overrideTaming: 'severe',
+  }),
+);
 
 // Even on non-v8, we tame the start compartment's Error constructor so
 // this assignment is not rejected, even if it does nothing.
