02f9a0338aa7fb3d3ff955f3a1cbe248d4f55a9f
Dan Connolly <dckc@madmode.com>
2021-03-26 21:57:08 -0500

    chore: revert ses import gyrations
    ... now that SES handles double initialization

--- ui/src/install-ses-lockdown.js
+++ ui/src/install-ses-lockdown.js
@@ -1,26 +1,11 @@
-/* global process */
-// import 'ses/lockdown';
+import 'ses/lockdown';
 import '@agoric/eventual-send/shim';
 
-function ifDevMode(thunk) {
-  if (typeof process !== 'undefined' && typeof process.env === 'object') {
-    console.log({ NODE_ENV: process.env.NODE_ENV });
-
-    if (process.env.NODE_ENV === 'development') {
-      thunk();
-    }
-  }
-}
-
-// In production, to avoid react-scripts / webpack rewriting SES,
-// we call lockdown() elsewhere. See ses-patch.js.
-ifDevMode(() =>
-  lockdown({
-    __allowUnsafeMonkeyPatching__: 'unsafe',
-    errorTaming: 'unsafe',
-    overrideTaming: 'severe',
-  }),
-);
+lockdown({
+  __allowUnsafeMonkeyPatching__: 'unsafe',
+  errorTaming: 'unsafe',
+  overrideTaming: 'severe',
+});
 
 // Even on non-v8, we tame the start compartment's Error constructor so
 // this assignment is not rejected, even if it does nothing.
