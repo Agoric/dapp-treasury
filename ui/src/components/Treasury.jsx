864a62e9cb4ed488a3318327add70d669658ae85
Kate Sills <kate@agoric.com>
2021-03-27 21:58:52 -0700

    chore: wire up manageVault page (#178)

--- ui/src/components/Treasury.jsx
+++ ui/src/components/Treasury.jsx
@@ -1,54 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// import { Link as RouterLink } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+
 import Divider from '@material-ui/core/Divider';
-// import List from '@material-ui/core/List';
-// import ListItem from '@material-ui/core/ListItem';
-// import ListItemIcon from '@material-ui/core/ListItemIcon';
-// import ListItemText from '@material-ui/core/ListItemText';
 
-// import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-// import Typography from '@material-ui/core/Typography';
-
-// import AirportShuttleIcon from '@material-ui/icons/AirportShuttle';
-// import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff';
-// import MonetizationIcon from '@material-ui/icons/MonetizationOn';
-// import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
-// import SwapIcon from '@material-ui/icons/SwapHoriz';
-// import TrendingUpIcon from '@material-ui/icons/TrendingUp';
-// import HowToVoteIcon from '@material-ui/icons/HowToVote';
+import Grid from '@material-ui/core/Grid';
 
 import { useApplicationContext } from '../contexts/Application';
 
 import { VaultSummary } from './VaultSummary';
 
-// const useStyles = makeStyles({
-//   root: {
-//     minWidth: 275,
-//   },
-//   bullet: {
-//     display: 'inline-block',
-//     margin: '0 2px',
-//     transform: 'scale(0.8)',
-//   },
-//   title: {
-//     fontSize: 14,
-//   },
-//   pos: {
-//     marginBottom: 12,
-//   },
-// });
+import { setVaultToManageId } from '../store';
 
 function VaultList() {
   const {
     state: { vaults },
+    dispatch,
   } = useApplicationContext();
+
+  const [redirect, setRedirect] = useState(false);
+
+  const handleOnClick = key => {
+    dispatch(setVaultToManageId(key));
+    setRedirect('/manageVault');
+  };
+
+  if (redirect) {
+    return <Redirect to={redirect} />;
+  }
   return (
     <div>
       <Divider />
@@ -60,8 +45,13 @@ function VaultList() {
                 <VaultSummary vault={v}></VaultSummary>
               </CardContent>
               <CardActions>
-                <Button size="small">Manage Debt</Button>
-                <Button size="small">Manage Collateral</Button>
+                <Grid container justify="flex-end">
+                  <Grid item>
+                    <Button size="small" onClick={() => handleOnClick(key)}>
+                      Manage Vault
+                    </Button>
+                  </Grid>
+                </Grid>
               </CardActions>
             </Card>
           </ListItem>
