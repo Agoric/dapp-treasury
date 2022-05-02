6da051710cf46b9652e34347257dda719152c18e
samsiegart <samsiegart@gmail.com>
2022-05-02 14:41:22 -0700

    chore: update config

--- ui/deploy/src/generated/defaults.js
+++ ui/deploy/src/generated/defaults.js
@@ -1,12 +1,7 @@
 export default {
-  "ON_CHAIN_CONFIG": [
-    "getAgoricNames",
-    [
-      "uiConfig",
-      "VaultFactory"
-    ]
-  ],
-  "AMM_NAME": "amm",
-  "BRIDGE_URL": "http://127.0.0.1:8000",
-  "CONTRACT_NAME": "VaultFactory"
+  ON_CHAIN_CONFIG: ['getAgoricNames', ['uiConfig', 'VaultFactory']],
+  BRIDGE_URL: 'http://127.0.0.1:8000',
+  CONTRACT_NAME: 'VaultFactory',
+  RUN_STAKE_ON_CHAIN_CONFIG: ['getAgoricNames', ['instance', 'runStake']],
+  RUN_STAKE_NAME: 'GetRUN',
 };
--- ui/src/generated/defaults.js
+++ ui/src/generated/defaults.js
@@ -1,8 +1,19 @@
 export default {
-  ON_CHAIN_CONFIG: ['getAgoricNames', ['uiConfig', 'VaultFactory']],
-  AMM_NAME: 'amm',
-  BRIDGE_URL: 'http://127.0.0.1:8000',
-  CONTRACT_NAME: 'VaultFactory',
-  RUN_STAKE_ON_CHAIN_CONFIG: ['getAgoricNames', ['instance', 'runStake']],
-  RUN_STAKE_NAME: 'GetRUN',
+  "ON_CHAIN_CONFIG": [
+    "getAgoricNames",
+    [
+      "uiConfig",
+      "VaultFactory"
+    ]
+  ],
+  "BRIDGE_URL": "http://127.0.0.1:8000",
+  "CONTRACT_NAME": "VaultFactory",
+  "RUN_STAKE_ON_CHAIN_CONFIG": [
+    "getAgoricNames",
+    [
+      "instance",
+      "runStake"
+    ]
+  ],
+  "RUN_STAKE_NAME": "GetRUN"
 };
